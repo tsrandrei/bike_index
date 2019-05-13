0d02b66ba7f8ba27fd81304417bd30a536337bec
Jake Romer <mail@jakeromer.com>
2019-05-13 17:56:43 -0400

    Add tasks table

--- app/controllers/organized/ambassadors_controller.rb
+++ app/controllers/organized/ambassadors_controller.rb
@@ -2,6 +2,7 @@ module Organized
   class AmbassadorsController < Organized::BaseController
     def index
       @ambassadors = User.ambassadors(organization: current_organization)
+      @tasks = []
     end
   end
 end
