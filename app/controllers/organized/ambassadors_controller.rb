c8a3d331704a6db66269bf369e5a5ceab8defb64
Jake Romer <mail@jakeromer.com>
2019-05-13 08:56:20 -0400

    List paginated bikes on stolen bikes page
    - Can't be marked as recovered or edited at all yet
    - Bikes need to be registered to the current organization to be edited by a
    - member of that organization

--- app/controllers/organized/ambassadors_controller.rb
+++ app/controllers/organized/ambassadors_controller.rb
@@ -1,6 +1,14 @@
 module Organized
   class AmbassadorsController < Organized::BaseController
+    include SortableTable
+
     def index
+    end
+
+    def stolen_bikes
+      @page = params[:page] || 1
+      @per_page = params[:per_page] || 25
+      @bikes = Bike.stolen.page(@page).per(@per_page)
     end
   end
 end
