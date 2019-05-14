a937fddef1559e14b17e1144a9f831c6c0aa1b72
Jake Romer <mail@jakeromer.com>
2019-05-14 14:16:53 -0400

    Fix AmbassadorTask tests

--- spec/models/ambassador_task_spec.rb
+++ spec/models/ambassador_task_spec.rb
@@ -2,6 +2,10 @@ require "spec_helper"
 
 describe AmbassadorTask, type: :model do
   it { is_expected.to respond_to(:description) }
-  it { is_expected.to respond_to(:completed) }
   it { is_expected.to respond_to(:users) }
+
+  it { is_expected.to have_many(:users) }
+  it { is_expected.to have_many(:ambassador_task_assignments) }
+
+  it { is_expected.to validate_uniqueness_of(:description) }
 end
